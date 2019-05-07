905f1990b16637f789e5d9f4abac62ba7868f666
shawnSFU <shawn.noruzi@gmail.com>
2019-05-06 22:04:39 -0700

    useEffect
    replaces lifecycle methods (componentDidMount/Unmount/Update).
    
    Runs on component render.
    
    Adding a 2nd argument for useEffect allows us to limit the function to only run when a specified variable gets changed.

--- src/index.js
+++ src/index.js
@@ -1,49 +1,64 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
 const NoteApp = () => {
-    const [notes, setNotes] = useState([])
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
-    const addNote = (e) => {  
-        e.preventDefault()
-        setNotes([
-            ...notes,
-            { title, body }
-        ])
-        setTitle('')
-        setBody('')
+  const addNote = e => {
+    e.preventDefault();
+    setNotes([...notes, { title, body }]);
+    setTitle("");
+    setBody("");
+  };
+
+  const removeNote = title => {
+    setNotes(notes.filter(note => note.title !== title));
+  };
+
+  //fetch + parse data  --- only runs once because of the empty array provided in the 2nd arg for useEffect
+  useEffect(() => {
+    const notesData = JSON.parse(localStorage.getItem("notes"));
+    if (notesData) {
+      setNotes(notesData);
     }
+    console.log('fetched')
+  }, []);
 
-    const removeNote = (title) => {
-        setNotes(notes.filter((note) => note.title !== title))
-    }
+  //change data when notes gets updated --- runs on every change 
+  useEffect(() => {
+    console.log('set')
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
-    return (
-        <div>
-            <h1>Notes</h1>
-            {notes.map((note) => (
-                <div key={note.title}>
-                    <h3>{note.title}</h3>
-                    <p>{note.body}</p>
-                    <button onClick={() => removeNote(note.title)}>x</button>
-                </div>
-            ))}
-            <p>Add note</p>
-            <form onSubmit={addNote}>
-                <input value={title} onChange={(e) => setTitle(e.target.value)} />
-                <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
-                <button>add note</button>
-            </form>
+  return (
+    <div>
+      <h1>Notes</h1>
+      {notes.map(note => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <p>{note.body}</p>
+          <button onClick={() => removeNote(note.title)}>x</button>
         </div>
-    )
-}
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={e => setTitle(e.target.value)} />
+        <textarea value={body} onChange={e => setBody(e.target.value)} />
+        <button>add note</button>
+      </form>
+    </div>
+  );
+};
 
 // const App = (props) => {
 //     const [count, setCount] = useState(props.count)
 //     const [text, setText] = useState('')
+//     useEffect(()=>{
+//         console.log('useEffect ran')
+//     })
 
 //     return (
 //         <div>
@@ -56,7 +71,7 @@ const NoteApp = () => {
 //     )
 // }
 
-ReactDOM.render(<NoteApp/>, document.getElementById('root'));
+ReactDOM.render(<NoteApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
